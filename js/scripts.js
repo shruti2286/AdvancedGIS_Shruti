4cd1234ca2366a0021249474ce1d3162229cb516
shruti2286 <47882761+shruti2286@users.noreply.github.com>
2020-02-06 19:59:51 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,14 @@ console.log(candyurl)
 
   $('#candy-photo').css('background-image', `url(${candyurl})`)
 })
+
+// data-types
+
+// loops can run code several times
+
+var timestorun = 10
+
+for(var i=0; i< timestorun; i++)
+{
+//code will run until the clause is no longer true
+}
